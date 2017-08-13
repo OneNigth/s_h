f721e6b58d0c8fb7c1da7a276d2848ab8cd4384e
OneNigth <602210386@qq.com>
2017-08-13 16:42:16 +0800

    form_commit_info
    表单验证信息

--- StudentManage/src/com/yj/action/UsersAction.java
+++ StudentManage/src/com/yj/action/UsersAction.java
@@ -1,5 +1,7 @@
 package com.yj.action;
 
+import org.apache.struts2.interceptor.validation.SkipValidation;
+
 import com.opensymphony.xwork2.ModelDriven;
 import com.yj.entity.Users;
 import com.yj.service.UsersDAO;
@@ -19,9 +21,28 @@ public class UsersAction extends SuperAction implements ModelDriven<Users> {
 		UsersDAO udao = new UsersDAOImpl();
 		// 登陆成功
 		if (udao.usersLogin(user)) {
+			session.setAttribute("loginUserName", user.getUsername());
 			return "login_success";
 		} else {
 			return "login_failure";
+		}
+	}
+
+	@SkipValidation
+	// 用户注销
+	public String logout() {
+		session.removeAttribute("loginUserName");
+		return "login_out";
+	}
+	
+	//表单验证
+	@Override
+	public void validate() {
+		if ("".equals(user.getUsername().trim())) {
+			this.addFieldError("usernameError", "用户名不能为空");
+		}
+		if (user.getPassword().length() < 6) {
+			this.addFieldError("passwordError", "密码长度不能小于6位");
 		}
 	}
 
